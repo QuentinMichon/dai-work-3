d7432112e72a3494d45e255d130c71f4023316a4
Quentin <quentin.michon@heig-vd.ch>
2025-12-13 15:27:51 +0100

    add type AvionJSON

---
+++ src/main/java/ch/heigvd/types/AvionJSON.java
@@ -0,0 +1,8 @@
+package ch.heigvd.types;
+
+public class AvionJSON {
+    public String constructor;
+    public String ICAO; // identifiant unique
+    public int range;
+    public int maxCapacity;
+}
